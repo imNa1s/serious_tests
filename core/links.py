c7b06faeb06259e7cbe9b6a32e2927fb73b90ffb
Na1s <imna1se@gmail.com>
2022-01-14 16:41:08 +0500

    add links for TDS and notify requests

---
+++ core/links.py
@@ -0,0 +1,6 @@
+class LinksReqTds:
+    tds_click_test = 'http://test1.clicktds.ru/CX1wS_0-qf?debug_operator_id=1&tds_no_redirect=1'
+
+
+class LinksReqNotify:
+    notify_test = 'http://test1.notify.clickpp.ru/mobbilling/index?'
