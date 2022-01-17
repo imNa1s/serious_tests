b0a3e6a47ad9c0d62138431220b7f1d39d4140fd
Na1s <imna1se@gmail.com>
2022-01-17 18:36:55 +0500

    init, ready test for login page

---
+++ tests/Test_login.py
@@ -0,0 +1,20 @@
+import time
+import pytest
+
+from core.links import SiteLinks
+from core.login_page import LoginPage
+
+
+@pytest.mark.login
+@pytest.mark.admin
+def test_login(browser):
+    link = SiteLinks.login_link_test1
+    link_check = SiteLinks.login_link_test1
+    Page = LoginPage(browser, link)
+    Page.open()
+    Page.write_email_admin()
+    Page.write_password_admin()
+    Page.login_button()
+    time.sleep(4)
+    to_check = browser.current_url
+    assert link_check != to_check, "you did't login"
