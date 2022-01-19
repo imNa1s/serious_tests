ce9495aa533381bd9491d560ebbac21c62bdecdf
Na1s <imna1se@gmail.com>
2022-01-19 13:46:28 +0500

    refactoring

--- tests/Test_sub.py
+++ tests/Test_sub.py
@@ -1,4 +1,4 @@
-from core.request_stf import Request_stuff, Request_methods
+from core.request_stf import Request_stuff, Request_urls
 from core.links import LinksReqTds
 from core.core_mt import BaseMt
 import requests
@@ -10,7 +10,7 @@ click_id = Request_stuff.click_id_take(LinksReqTds.tds_click_test)
 @pytest.mark.sub
 @pytest.mark.on
 def test_sub():
-    request_link = Request_methods.sub_on_request_url(click_id)
+    request_link = Request_urls.sub_on_request_url(click_id)
     request = BaseMt.just_click(request_link)
     assert request.status_code == requests.codes.ok, f'sub didn\'t done, status code = {request.status_code}"'
 
@@ -18,7 +18,7 @@ def test_sub():
 @pytest.mark.sub
 @pytest.mark.off
 def test_unsub():
-    request_link = Request_methods.sub_off_request_url(click_id)
+    request_link = Request_urls.sub_off_request_url(click_id)
     request = BaseMt.just_click(request_link)
     assert request.status_code == requests.codes.ok, f'unsub didn\'t done, status code = {request.status_code}"'
 
@@ -26,6 +26,6 @@ def test_unsub():
 @pytest.mark.sub
 @pytest.mark.rebill
 def test_rebill():
-    request_link = Request_methods.sub_rebill_request_url(click_id)
+    request_link = Request_urls.sub_rebill_request_url(click_id)
     request = BaseMt.just_click(request_link)
     assert request.status_code == requests.codes.ok, f'resub didn\'t done, status code = {request.status_code}"'
