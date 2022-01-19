a337aab0b521a99926ba9ecab6946e5ac7f98e19
Na1s <imna1se@gmail.com>
2022-01-19 13:35:34 +0500

    add locators for statistic

--- core/locators.py
+++ core/locators.py
@@ -6,3 +6,8 @@ class LoginLocators:
     password_id = (By.CSS_SELECTOR, '#loginform-password')
     captcha_id = (By.XPATH, '//*[@id="recaptcha-anchor"]')
     button_submit = (By.CSS_SELECTOR, '[name="login-button"]')
+
+
+class StatsLocators:
+    statistic_button = (By.CSS_SELECTOR, '#main-menu > li:nth-child(2)')
+    main_statistic = (By.CSS_SELECTOR, '#main-menu > li.active > ul > li:nth-child(1) > a > span')
