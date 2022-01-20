71958ebeb274aa51cd24dd721c22a5cfe1becfad
Na1s <imna1se@gmail.com>
2022-01-20 19:17:46 +0500

    add single tests for columns: date, transition, unic, traffback, sub, unsub,rebill

--- tests/Test_statistic.py
+++ tests/Test_statistic.py
@@ -3,13 +3,67 @@ import pytest
 
 from core.links import SiteLinks
 from core.ready_function import login_pass
-from core.ready_function import transition
+from core.take_all_stats import StatisticsMt
+
+
+@pytest.mark.stats
+@pytest.mark.date
+def test_statistic_date(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.stats_date(browser, browser.current_url)
+    time.sleep(1)
 
 
 @pytest.mark.stats
 @pytest.mark.transition
-def test_transitions(browser):
+# def test_transitions(browser):
+#     link = SiteLinks.login_link_test1
+#     login_pass(browser, link)
+#     StatisticsMt.transition(browser, browser.current_url)
+#     time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.unic
+def test_unic(browser):
     link = SiteLinks.login_link_test1
     login_pass(browser, link)
-    transition(browser, browser.current_url)
+    StatisticsMt.unic_request(browser, browser.current_url)
+    time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.traffback
+def test_traffback(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.traffback_request(browser, browser.current_url)
+    time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.subscribe
+def test_subscribe(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.subscribe(browser, browser.current_url)
+    time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.unsub
+def test_unsub(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.unsub(browser, browser.current_url)
+    time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.rebill
+def test_rebill(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.make_rebill(browser, browser.current_url)
     time.sleep(1)
