7bbd9f167d7c846381ff9b98bdf8f209c09c5273
Na1s <imna1se@gmail.com>
2022-01-20 19:12:56 +0500

    add link to ransom

--- core/links.py
+++ core/links.py
@@ -1,5 +1,6 @@
 class LinksReqTds:
     tds_click_test = 'http://test1.clicktds.ru/CX1wS_0-qf?debug_operator_id=1&tds_no_redirect=1'
+    tds_click_test_1 = 'http://test1.clicktds.ru/-NHVooUv-w?debug_operator_id=1&tds_no_redirect=1'
     tds_click_test_traffback = 'http://test1.clicktds.ru/CX1wS_0-qf'
 
 
