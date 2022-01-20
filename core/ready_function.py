d407f9ab545259a51c6795d21715bf0a76cf2052
Na1s <imna1se@gmail.com>
2022-01-20 16:03:43 +0500

    transfer transition in separate category of functions, add traffback

--- core/ready_function.py
+++ core/ready_function.py
@@ -6,7 +6,6 @@ from core.core_mt import BaseMt
 from core.links import SiteLinks, LinksReqTds
 from core.login_page import LoginPage
 from core.request_stf import Request_stuff, Request_urls
-from core.statistc_base import StatsMt
 
 
 def login_pass(browser, link):
@@ -21,22 +20,13 @@ def login_pass(browser, link):
     assert link_check != to_check, "you did't login"
 
 
-def transition(browser, link):
-    Page = StatsMt(browser, link)
-    Page.go_to_statistic()
-    stats = Page.save_stats()
-    stats_pars1 = Page.transition_take(stats)
-    sub_on()
-    time.sleep(20)
-    Page.ref()
-    stats = Page.save_stats()
-    stats_pars2 = Page.transition_take(stats)
-    print(f'transition before request {stats_pars1}, after request {stats_pars2}')
-    assert stats_pars1 != stats_pars2, "they equal"
-
-
 def sub_on():
     click_id = Request_stuff.click_id_take(LinksReqTds.tds_click_test)
     request_link = Request_urls.sub_on_request_url(click_id)
     request = BaseMt.just_click(request_link)
+    assert request.status_code == requests.codes.ok, f'sub didn\'t done, status code = {request.status_code}"'
+
+
+def traffback():
+    request = BaseMt.just_click(LinksReqTds.tds_click_test_traffback)
     assert request.status_code == requests.codes.ok, f'sub didn\'t done, status code = {request.status_code}"'
