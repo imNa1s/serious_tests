b0cdb4e008174c4df0827dc822906cac6871bd11
Na1s <imna1se@gmail.com>
2022-01-24 12:19:03 +0500

    refactoring trafficback link

--- core/links.py
+++ core/links.py
@@ -1,7 +1,7 @@
 class LinksReqTds:
     tds_click_test = 'http://test1.clicktds.ru/CX1wS_0-qf?debug_operator_id=1&tds_no_redirect=1'
     tds_click_test_1 = 'http://test1.clicktds.ru/-NHVooUv-w?debug_operator_id=1&tds_no_redirect=1'
-    tds_click_test_traffback = 'http://test1.clicktds.ru/CX1wS_0-qf'
+    tds_click_test_traffback = 'http://test1.clicktds.ru/TIpWCGIljV'
 
 
 class LinksReqNotify:
