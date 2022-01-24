9c6f194eac42981bb4b0079e5b5b9841084ac6a8
Na1s <imna1se@gmail.com>
2022-01-24 12:27:17 +0500

    refactoring tets - traffback, subscribe, add not not fully prepared tests - NK, epc, partner, system_pay, all_pay, complaints

--- tests/Test_statistic.py
+++ tests/Test_statistic.py
@@ -4,6 +4,7 @@ import pytest
 from core.links import SiteLinks
 from core.ready_function import login_pass
 from core.take_all_stats import StatisticsMt
+from tr import StatisticsMt1
 
 
 @pytest.mark.stats
@@ -17,11 +18,11 @@ def test_statistic_date(browser):
 
 @pytest.mark.stats
 @pytest.mark.transition
-# def test_transitions(browser):
-#     link = SiteLinks.login_link_test1
-#     login_pass(browser, link)
-#     StatisticsMt.transition(browser, browser.current_url)
-#     time.sleep(1)
+def test_transitions(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.transition(browser, browser.current_url)
+    time.sleep(1)
 
 
 @pytest.mark.stats
@@ -52,6 +53,15 @@ def test_subscribe(browser):
 
 
 @pytest.mark.stats
+@pytest.mark.conversion
+def test_conversion(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.conversion(browser, browser.current_url)
+    time.sleep(1)
+
+
+@pytest.mark.stats
 @pytest.mark.unsub
 def test_unsub(browser):
     link = SiteLinks.login_link_test1
@@ -67,3 +77,75 @@ def test_rebill(browser):
     login_pass(browser, link)
     StatisticsMt.make_rebill(browser, browser.current_url)
     time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.buyout
+def test_buyout(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.buyout(browser, browser.current_url)
+    time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.nk
+def test_nk(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.NK(browser, browser.current_url)
+    time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.epc
+def test_epc(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.EPC(browser, browser.current_url)
+    time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.partner
+def test_partner(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.partner(browser, browser.current_url)
+    time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.system_pay
+def test_system_pay(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.system_pay(browser, browser.current_url)
+    time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.all_pay
+def test_all_pay(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.all_pay(browser, browser.current_url)
+    time.sleep(1)
+
+
+@pytest.mark.stats
+@pytest.mark.complaints
+def test_complaints(browser):
+    link = SiteLinks.login_link_test1
+    login_pass(browser, link)
+    StatisticsMt.complaints(browser, browser.current_url)
+    time.sleep(1)
+
+
+# @pytest.mark.stats
+# @pytest.mark.complaints
+# def test_complaints(browser):
+#     link = SiteLinks.login_link_test1
+#     login_pass(browser, link)
+#     StatisticsMt1.all_statistic_in_one(browser, browser.current_url)
+#     time.sleep(1)
