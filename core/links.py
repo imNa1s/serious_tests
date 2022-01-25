5798e47dabbf8f01d7f48a6e44373df3ee7c1ddc
Na1s <imna1se@gmail.com>
2022-01-25 18:48:35 +0500

    add postback link

--- core/links.py
+++ core/links.py
@@ -2,6 +2,9 @@ class LinksReqTds:
     tds_click_test = 'http://test1.clicktds.ru/CX1wS_0-qf?debug_operator_id=1&tds_no_redirect=1'
     tds_click_test_1 = 'http://test1.clicktds.ru/-NHVooUv-w?debug_operator_id=1&tds_no_redirect=1'
     tds_click_test_traffback = 'http://test1.clicktds.ru/TIpWCGIljV'
+    postback = 'http://tds.click/0f5ee17/postback?subid={extra_param}&status={action}&tid={transaction_id}&payout={' \
+               'income}&number={msisdn}&label1={label1}&label2={label2}&time={time}&stream_id={' \
+               'stream_id}&landing_id={landing_id}&operator={operator} '
 
 
 class LinksReqNotify:
@@ -10,3 +13,4 @@ class LinksReqNotify:
 
 class SiteLinks:
     login_link_test1 = 'http://test1.admin.clickpp.ru/site/login'
+
