5957544d0621fe765b4608409cf1f6c40028fc3f
Na1s <imna1se@gmail.com>
2022-02-03 12:16:47 +0500

    add login fail test

--- tests/Test_login.py
+++ tests/Test_login.py
@@ -18,3 +18,18 @@ def test_login(browser):
     time.sleep(4)
     to_check = browser.current_url
     assert link_check != to_check, "you did't login"
+
+
+@pytest.mark.login_fail
+@pytest.mark.admin
+def test_login_fail(browser):
+    link = SiteLinks.login_link_test1
+    link_check = SiteLinks.login_link_test1
+    Page = LoginPage(browser, link)
+    Page.open()
+    Page.write_email_admin()
+    Page.write_password_admin()
+    Page.login_button()
+    time.sleep(4)
+    to_check = browser.current_url
+    assert link_check != to_check, "you login"
