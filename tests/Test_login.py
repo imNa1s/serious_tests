91b7ae28f0b78b3ea140b9bce4b16be04ff1fc44
Na1s <imna1se@gmail.com>
2022-02-03 13:50:37 +0500

    refactoring marks, add test for login as partner

--- tests/Test_login.py
+++ tests/Test_login.py
@@ -3,6 +3,8 @@ import pytest
 
 from core.links import SiteLinks
 from core.login_page import LoginPage
+from core.partner_page import GoToPartnerPage
+from core.ready_function import BrowserMt
 
 
 @pytest.mark.login
@@ -20,8 +22,8 @@ def test_login(browser):
     assert link_check != to_check, "you did't login"
 
 
-@pytest.mark.login_fail
-@pytest.mark.admin
+@pytest.mark.login
+@pytest.mark.admin_fail
 def test_login_fail(browser):
     link = SiteLinks.login_link_test1
     link_check = SiteLinks.login_link_test1
@@ -33,3 +35,17 @@ def test_login_fail(browser):
     time.sleep(4)
     to_check = browser.current_url
     assert link_check != to_check, "you login"
+
+
+@pytest.mark.login
+@pytest.mark.admin_partner
+def test_login_as_partner(browser):
+    link = SiteLinks.login_link_test1
+    BrowserMt.login_pass(browser, link)
+    Page = GoToPartnerPage(browser, link)
+    Page.partner_button()
+    Page.testmail_parnter()
+    check_link = browser.current_url
+    Page.testmail_autorization()
+    to_check = browser.current_url
+    assert check_link != to_check, "you didn't login as partner"
