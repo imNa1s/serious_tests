d6ea79755b3f7e285ebca28f963bfada54f4a05e
Na1s <imna1se@gmail.com>
2022-02-08 13:36:35 +0500

    split class from file on two new file

--- core/ready_page_mt.py
+++ core/ready_page_mt.py
@@ -1,11 +1,7 @@
 import time
 
-import requests
-
-from core.core_mt import BaseMt
 from core.links import SiteLinks
 from core.login_page import LoginPage
-from core.request_stf import Request_stuff, Request_urls
 from core.partner_page import GoToPartnerPage, PartnerNavBorder, PartnerCreateSource, PartnerCreateStream
 
 
@@ -56,35 +52,4 @@ class BrowserMt:
         Page.stream_create_button()
 
 
-class SubscribeMt:
-    def redirect_without_sub(self):
-        request = BaseMt.just_click(self)
-        assert request.status_code == requests.codes.ok, f'sub didn\'t done, status code = {request.status_code}"'
 
-    def sub_on(self):
-        click_id = Request_stuff.click_id_take(self)
-        request_link = Request_urls.sub_on_request_url(click_id)
-        request = BaseMt.just_click(request_link)
-        assert request.status_code == requests.codes.ok, f'sub didn\'t done, status code = {request.status_code}"'
-
-    def rebill(self):
-        click_id = Request_stuff.click_id_take(self)
-        request_link = Request_urls.sub_on_request_url(click_id)
-        BaseMt.just_click(request_link)
-        time.sleep(20)
-        request_link = Request_urls.sub_rebill_request_url(click_id)
-        request = BaseMt.just_click(request_link)
-        assert request.status_code == requests.codes.ok, f'sub didn\'t done, status code = {request.status_code}"'
-
-    def traffback(self):
-        request = BaseMt.just_click(self)
-        assert request.status_code == requests.codes.ok, f'sub didn\'t done, status code = {request.status_code}"'
-
-    def sub_off(self):
-        click_id = Request_stuff.click_id_take(self)
-        request_link = Request_urls.sub_on_request_url(click_id)
-        BaseMt.just_click(request_link)
-        time.sleep(20)
-        request_link = Request_urls.sub_off_request_url(click_id)
-        request = BaseMt.just_click(request_link)
-        assert request.status_code == requests.codes.ok, f'sub didn\'t done, status code = {request.status_code}"'
