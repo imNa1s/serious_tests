5a67cb1a87bf83f08c8caaa4156b4946cc518dfe
Na1s <imna1se@gmail.com>
2022-02-09 16:25:55 +0500

    add methods for tikets

--- core/ready_page_mt.py
+++ core/ready_page_mt.py
@@ -2,7 +2,7 @@ import time
 
 from core.links import SiteLinks
 from core.login_page import LoginPage
-from core.partner_page import GoToPartnerPage, PartnerNavBorder, PartnerCreateSource, PartnerCreateStream
+from core.partner_page import GoToPartnerPage, PartnerNavBorder, PartnerCreateSource, PartnerCreateStream, PartnerTicketsMt
 
 
 class BrowserMt:
@@ -51,5 +51,19 @@ class BrowserMt:
         Page.add_land_button()
         Page.stream_create_button()
 
-
-
+    def create_tiket(self, link):
+        Page = PartnerNavBorder(self, link)
+        Page.partner_tickets()
+        Page = PartnerTicketsMt(self, link)
+        Page.switch_to_old_win()
+        alert_icon_bef = Page.see_alert()
+        Page.switch_to_new_win()
+        Page.ticket_create()
+        Page.ticket_type()
+        Page.ticket_type_message()
+        Page.ticket_message()
+        Page.ticket_send_btn()
+        Page.switch_to_old_win()
+        Page.ref()
+        alert_icon_af = Page.see_alert()
+        assert alert_icon_bef != alert_icon_af, 'no new alerts'
