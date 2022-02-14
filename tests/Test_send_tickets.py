6d3472df859f00eb7f8c24f0cce9aca2ebf2e73e
Na1s <imna1se@gmail.com>
2022-02-14 15:18:34 +0500

    refactoring test names, add function for test partner alert

--- tests/Test_send_tickets.py
+++ tests/Test_send_tickets.py
@@ -8,8 +8,17 @@ from core.ready_page_mt import BrowserMt
 @pytest.mark.test1
 @pytest.mark.ticket
 @pytest.mark.alert
-def test_ticket_alert(browser):
+def test_ticket_alert_admin(browser):
     link = SiteLinks.login_link_test1
     BrowserMt.login_pass(browser, link)
     BrowserMt.partner_redirect(browser, link)
     TicketMt.create_tiket(browser, link)
+
+
+@pytest.mark.test1
+@pytest.mark.ticket
+@pytest.mark.alert
+def test_ticket_alert_partner(browser):
+    link = SiteLinks.login_link_test1
+    BrowserMt.login_pass(browser, link)
+    TicketMt.create_adm_ticket(browser, link)
