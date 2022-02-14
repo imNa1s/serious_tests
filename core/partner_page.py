586f8149da8ce2b33305e14445cc8e5dba4b138d
Na1s <imna1se@gmail.com>
2022-02-14 12:57:43 +0500

    refactoring because we split partnerlocators class on two

--- core/partner_page.py
+++ core/partner_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support.ui import Select
 
 from core.core_mt import BaseMt
 from core.links import LinksReqTds
-from core.locators import PartnerLocators
+from core.locators import PartnerLocators, StreamLocators, TicketLocators
 from core.statistic.main_statistic_base import StatsMt
 
 
@@ -31,8 +31,8 @@ class PartnerNavBorder(BaseMt, PartnerLocators):
         self.find_el_click(*PartnerLocators.partner_source)
 
     def partner_stream(self):
-        assert self.is_element_present(*PartnerLocators.partner_stream), 'can\'t find button stream'
-        self.find_el_click(*PartnerLocators.partner_stream)
+        assert self.is_element_present(*StreamLocators.partner_stream), 'can\'t find button stream'
+        self.find_el_click(*StreamLocators.partner_stream)
 
     def partner_tickets(self):
         assert self.is_element_present(*PartnerLocators.partner_tickets), 'can\'t find button tickets'
@@ -65,111 +65,111 @@ class PartnerCreateSource(BaseMt, PartnerLocators):
 
 class PartnerCreateStream(BaseMt, PartnerLocators):
     def partner_stream_create(self):
-        assert self.is_element_present(*PartnerLocators.create_stream), 'can\'t find button create stream'
-        self.find_el_click(*PartnerLocators.create_stream)
+        assert self.is_element_present(*StreamLocators.create_stream), 'can\'t find button create stream'
+        self.find_el_click(*StreamLocators.create_stream)
 
     def partner_stream_name(self):
-        assert self.is_element_present(*PartnerLocators.stream_name), 'can\'t find field to name'
-        self.find_el_write(*PartnerLocators.stream_name, 'autotest')
+        assert self.is_element_present(*StreamLocators.stream_name), 'can\'t find field to name'
+        self.find_el_write(*StreamLocators.stream_name, 'autotest')
 
     def stream_source(self):
-        assert self.is_element_present(*PartnerLocators.stream_source), 'can\'t find field source'
-        select = Select(self.browser.find_element(*PartnerLocators.stream_source))
+        assert self.is_element_present(*StreamLocators.stream_source), 'can\'t find field source'
+        select = Select(self.browser.find_element(*StreamLocators.stream_source))
         select.select_by_value('number:714')
 
     def stream_traffback_url(self):
-        assert self.is_element_present(*PartnerLocators.traffback_url), 'can\'t find field traffback'
-        self.find_el_clear(*PartnerLocators.traffback_url)
-        self.find_el_write(*PartnerLocators.traffback_url, 'http://ya.ru')
+        assert self.is_element_present(*StreamLocators.traffback_url), 'can\'t find field traffback'
+        self.find_el_clear(*StreamLocators.traffback_url)
+        self.find_el_write(*StreamLocators.traffback_url, 'http://ya.ru')
 
     def stream_postback(self):
-        assert self.is_element_present(*PartnerLocators.postback_url), 'can\'t find field postback'
-        self.find_el_write(*PartnerLocators.postback_url, f'{LinksReqTds.postback}')
+        assert self.is_element_present(*StreamLocators.postback_url), 'can\'t find field postback'
+        self.find_el_write(*StreamLocators.postback_url, f'{LinksReqTds.postback}')
 
     def postback_actions_all(self):
-        assert self.is_element_present(*PartnerLocators.postback_check_sub), 'can\'t find checkbox sub'
-        self.find_el_click(*PartnerLocators.postback_check_sub)
-        assert self.is_element_present(*PartnerLocators.postback_check_rebill), 'can\'t find checkbox rebill'
-        self.find_el_click(*PartnerLocators.postback_check_rebill)
-        assert self.is_element_present(*PartnerLocators.postback_check_unsub), 'can\'t find checkbox unsub'
-        self.find_el_click(*PartnerLocators.postback_check_unsub)
-        assert self.is_element_present(*PartnerLocators.postback_check_buyout), 'can\'t find checkbox buyout'
-        self.find_el_click(*PartnerLocators.postback_check_buyout)
+        assert self.is_element_present(*StreamLocators.postback_check_sub), 'can\'t find checkbox sub'
+        self.find_el_click(*StreamLocators.postback_check_sub)
+        assert self.is_element_present(*StreamLocators.postback_check_rebill), 'can\'t find checkbox rebill'
+        self.find_el_click(*StreamLocators.postback_check_rebill)
+        assert self.is_element_present(*StreamLocators.postback_check_unsub), 'can\'t find checkbox unsub'
+        self.find_el_click(*StreamLocators.postback_check_unsub)
+        assert self.is_element_present(*StreamLocators.postback_check_buyout), 'can\'t find checkbox buyout'
+        self.find_el_click(*StreamLocators.postback_check_buyout)
 
     def postback_actions_sub(self):
-        assert self.is_element_present(*PartnerLocators.postback_check_sub), 'can\'t find checkbox sub'
-        self.find_el_click(*PartnerLocators.postback_check_sub)
+        assert self.is_element_present(*StreamLocators.postback_check_sub), 'can\'t find checkbox sub'
+        self.find_el_click(*StreamLocators.postback_check_sub)
 
     def postback_actions_rebill(self):
-        assert self.is_element_present(*PartnerLocators.postback_check_rebill), 'can\'t find checkbox rebill'
-        self.find_el_click(*PartnerLocators.postback_check_rebill)
+        assert self.is_element_present(*StreamLocators.postback_check_rebill), 'can\'t find checkbox rebill'
+        self.find_el_click(*StreamLocators.postback_check_rebill)
 
     def postback_actions_unsub(self):
-        assert self.is_element_present(*PartnerLocators.postback_check_unsub), 'can\'t find checkbox unsub'
-        self.find_el_click(*PartnerLocators.postback_check_unsub)
+        assert self.is_element_present(*StreamLocators.postback_check_unsub), 'can\'t find checkbox unsub'
+        self.find_el_click(*StreamLocators.postback_check_unsub)
 
     def postback_actions_buyout(self):
-        assert self.is_element_present(*PartnerLocators.postback_check_buyout), 'can\'t find checkbox buyout'
-        self.find_el_click(*PartnerLocators.postback_check_buyout)
+        assert self.is_element_present(*StreamLocators.postback_check_buyout), 'can\'t find checkbox buyout'
+        self.find_el_click(*StreamLocators.postback_check_buyout)
 
     def type_stream_redirect(self):
-        assert self.is_element_present(*PartnerLocators.redirect_type_simple), 'can\'t find stream redirect settings'
-        self.find_el_click(*PartnerLocators.redirect_type_simple)
+        assert self.is_element_present(*StreamLocators.redirect_type_simple), 'can\'t find stream redirect settings'
+        self.find_el_click(*StreamLocators.redirect_type_simple)
 
     def payment_scheme(self):
-        assert self.is_element_present(*PartnerLocators.auto_buyout), 'can\'t find checkbox auto buyout'
-        self.find_el_click(*PartnerLocators.auto_buyout)
+        assert self.is_element_present(*StreamLocators.auto_buyout), 'can\'t find checkbox auto buyout'
+        self.find_el_click(*StreamLocators.auto_buyout)
 
     def landing_page_display(self):
-        assert self.is_element_present(*PartnerLocators.view_land_round), 'can\'t find landing display settings'
-        self.find_el_click(*PartnerLocators.view_land_round)
+        assert self.is_element_present(*StreamLocators.view_land_round), 'can\'t find landing display settings'
+        self.find_el_click(*StreamLocators.view_land_round)
 
     def ya_redirect_ban(self):
-        assert self.is_element_present(*PartnerLocators.ban_ya_redirect), 'can\'t find yandex ban settings'
-        self.find_el_click(*PartnerLocators.ban_ya_redirect)
+        assert self.is_element_present(*StreamLocators.ban_ya_redirect), 'can\'t find yandex ban settings'
+        self.find_el_click(*StreamLocators.ban_ya_redirect)
 
     def landing_mts(self):
-        assert self.is_element_present(*PartnerLocators.landing_mts), 'can\'t find MTS'
-        self.find_el_click(*PartnerLocators.landing_mts)
+        assert self.is_element_present(*StreamLocators.landing_mts), 'can\'t find MTS'
+        self.find_el_click(*StreamLocators.landing_mts)
 
     def choice_iplayer(self):
-        assert self.is_element_present(*PartnerLocators.landing_iplayer), 'can\'t find iplayer'
-        self.find_el_click(*PartnerLocators.landing_iplayer)
+        assert self.is_element_present(*StreamLocators.landing_iplayer), 'can\'t find iplayer'
+        self.find_el_click(*StreamLocators.landing_iplayer)
 
     def add_land_button(self):
-        assert self.is_element_present(*PartnerLocators.add_lending_btn), 'can\'t find add landing button'
-        self.find_el_click(*PartnerLocators.add_lending_btn)
+        assert self.is_element_present(*StreamLocators.add_lending_btn), 'can\'t find add landing button'
+        self.find_el_click(*StreamLocators.add_lending_btn)
 
     def stream_create_button(self):
-        assert self.is_element_present(*PartnerLocators.create_stream_btn), 'can\'t find button create stream'
-        self.find_el_click(*PartnerLocators.create_stream_btn)
+        assert self.is_element_present(*StreamLocators.create_stream_btn), 'can\'t find button create stream'
+        self.find_el_click(*StreamLocators.create_stream_btn)
 
 
-class PartnerTicketsMt(StatsMt, PartnerLocators):
+class PartnerTicketsMt(StatsMt, StreamLocators):
     def ticket_create(self):
-        assert self.is_element_present(*PartnerLocators.create_button_ticket), 'can\'t find button create ticket'
-        self.find_el_click(*PartnerLocators.create_button_ticket)
+        assert self.is_element_present(*TicketLocators.create_button_ticket), 'can\'t find button create ticket'
+        self.find_el_click(*TicketLocators.create_button_ticket)
 
     def ticket_type(self):
-        assert self.is_element_present(*PartnerLocators.ticket_type_select), 'can\'t find field source'
-        select = Select(self.browser.find_element(*PartnerLocators.ticket_type_select))
+        assert self.is_element_present(*TicketLocators.ticket_type_select), 'can\'t find field source'
+        select = Select(self.browser.find_element(*TicketLocators.ticket_type_select))
         select.select_by_value('1')
 
     def ticket_type_message(self):
-        assert self.is_element_present(*PartnerLocators.ticket_message_type), 'can\'t find field massage type'
-        self.find_el_write(*PartnerLocators.ticket_message_type, 'autotest')
+        assert self.is_element_present(*TicketLocators.ticket_message_type), 'can\'t find field massage type'
+        self.find_el_write(*TicketLocators.ticket_message_type, 'autotest')
 
     def ticket_message(self):
-        assert self.is_element_present(*PartnerLocators.ticket_message), 'can\'t find field massage'
-        self.find_el_write(*PartnerLocators.ticket_message, 'autotest')
+        assert self.is_element_present(*TicketLocators.ticket_message), 'can\'t find field massage'
+        self.find_el_write(*TicketLocators.ticket_message, 'autotest')
 
     def ticket_send_btn(self):
-        assert self.is_element_present(*PartnerLocators.btn_send_ticket), 'can\'t find button send ticket'
-        self.find_el_click(*PartnerLocators.btn_send_ticket)
+        assert self.is_element_present(*TicketLocators.btn_send_ticket), 'can\'t find button send ticket'
+        self.find_el_click(*TicketLocators.btn_send_ticket)
 
     def see_alert(self):
-        assert self.is_element_present(*PartnerLocators.alert_ticket_icon), 'no field alerts'
-        how_much = self.browser.find_element(*PartnerLocators.alert_ticket_icon)
+        assert self.is_element_present(*TicketLocators.alert_ticket_icon), 'no field alerts'
+        how_much = self.browser.find_element(*TicketLocators.alert_ticket_icon)
         return how_much.text
 
 
