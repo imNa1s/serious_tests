cc41eac77204431f0a0a016483c60091eb96386f
Na1s <imna1se@gmail.com>
2022-02-14 12:59:29 +0500

    refactoring

--- tests/Test_send_tickets.py
+++ tests/Test_send_tickets.py
@@ -1,6 +1,7 @@
 import pytest
 
 from core.links import SiteLinks
+from core.Page_methods.ticket_page import TicketMt
 from core.ready_page_mt import BrowserMt
 
 
@@ -11,4 +12,4 @@ def test_ticket_alert(browser):
     link = SiteLinks.login_link_test1
     BrowserMt.login_pass(browser, link)
     BrowserMt.partner_redirect(browser, link)
-    BrowserMt.create_tiket(browser, link)
+    TicketMt.create_tiket(browser, link)
