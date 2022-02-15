99d658c03b381cec93c9fc31b1a4c85e32385455
Na1s <imna1se@gmail.com>
2022-02-15 17:27:51 +0500

    refactoring methods add new to take 1st day unsub stats

--- core/statistic/sub_statistic_base.py
+++ core/statistic/sub_statistic_base.py
@@ -12,14 +12,14 @@ class TakeSubStatsMt(BaseMt, StatsLocators):
         self.find_el_click(*StatsLocators.sub_statistic)
 
     def sub_date_take(self, soup):
-        table = soup.find_all('tbody')[2]
+        table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[0]
         table = table.find_all('td')[0]
         table = table.get_text()
         return table
 
     def sub_take(self, soup):
-        table = soup.find_all('tbody')[2]
+        table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[0]
         table = table.find_all('td')[1]
         table = table.find_all('span')[0]
@@ -27,78 +27,85 @@ class TakeSubStatsMt(BaseMt, StatsLocators):
         return table
 
     def sub_active_take(self, soup):
-        table = soup.find_all('tbody')[2]
+        table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[0]
         table = table.find_all('td')[2]
         table = table.get_text()
         return table
 
     def unsub_take(self, soup):
-        table = soup.find_all('tbody')[2]
+        table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[0]
         table = table.find_all('td')[3]
         table = table.get_text()
         return table
 
-    def unsub_1st_day_take(self, soup):
-        table = soup.find_all('tbody')[2]
+    def unsub_1st_30min_take(self, soup):
+        table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[0]
         table = table.find_all('td')[4]
         table = table.get_text()
         return table
 
-    def sub_rebill_take(self, soup):
-        table = soup.find_all('tbody')[2]
+    def unsub_1st_day_take(self, soup):
+        table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[0]
         table = table.find_all('td')[5]
         table = table.get_text()
         return table
 
-    def rebill_on_abonent_take(self, soup):
+    def sub_rebill_take(self, soup):
         table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[0]
         table = table.find_all('td')[6]
         table = table.get_text()
         return table
 
-    def sub_price_take(self, soup):
+    def rebill_on_abonent_take(self, soup):
         table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[0]
         table = table.find_all('td')[7]
         table = table.get_text()
         return table
 
-    def sub_buyout_take(self, soup):
-        table = soup.find_all('tbody')[2]
+    def sub_price_take(self, soup):
+        table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[0]
         table = table.find_all('td')[8]
+        table = table.get_text()
+        return table
+
+    def sub_buyout_take(self, soup):
+        table = soup.find_all('tbody')[1]
+        table = table.find_all('tr')[0]
+        table = table.find_all('td')[9]
         table = table.get_text()
         return table
 
     def sub_rebill_buyout_take(self, soup):
         table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[0]
-        table = table.find_all('td')[9]
+        table = table.find_all('td')[10]
         table = table.get_text()
         return table
 
     def sub_partner_take(self, soup):
-        table = soup.find_all('tbody')[2]
-        table = table.find_all('tr')[0]
-        table = table.find_all('td')[9]
-        table = table.get_text()
-        return table
-
-    def sub_system_take(self, soup):
         table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[0]
         table = table.find_all('td')[11]
         table = table.get_text()
         return table
 
+    def sub_system_take(self, soup):
+        table = soup.find_all('tbody')[1]
+        table = table.find_all('tr')[0]
+        table = table.find_all('td')[12]
+        table = table.get_text()
+        return table
+
     def sub_all_pay_take(self, soup):
-        table = soup.find_all('tbody')[2]
+        table = soup.find_all('tbody')[1]
         table = table.find_all('tr')[1]
-        table = table.find_all('td')[11]
+        table = table.find_all('td')[13]
         table = table.get_text()
         return table
