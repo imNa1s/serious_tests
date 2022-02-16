7ff2ca2941ae7edf2750f832b9ea7a0bbf527fdf
Na1s <imna1se@gmail.com>
2022-02-16 20:20:02 +0500

    refactoring delete extra imports

--- core/Page_methods/partner_page.py
+++ core/Page_methods/partner_page.py
@@ -1,7 +1,4 @@
-
-
 from core.core_mt import BaseMt
-from core.links import LinksReqTds
 from core.locators import PartnerLocators, StreamLocators
 from core.statistic.main_statistic_base import StatsMt
 
