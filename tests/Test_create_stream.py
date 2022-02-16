127f848aeeca7734e381c02abd5cb88c9904fb00
Na1s <imna1se@gmail.com>
2022-02-16 20:23:52 +0500

    update imports

--- tests/Test_create_stream.py
+++ tests/Test_create_stream.py
@@ -1,5 +1,6 @@
 import pytest
 
+from core.Page_methods.stream_ready_mt import StreamReadyMt
 from core.links import SiteLinks, LinksToCheck
 from core.ready_page_mt import BrowserMt
 
@@ -10,6 +11,6 @@ def test_create_stream(browser):
     link = SiteLinks.login_link_test1
     BrowserMt.login_pass(browser, link)
     BrowserMt.partner_redirect(browser, link)
-    BrowserMt.create_stream(browser, link)
+    StreamReadyMt.create_stream(browser, link)
     check = browser.current_url
     assert check != LinksToCheck.create_stream_link, 'stream not created'
