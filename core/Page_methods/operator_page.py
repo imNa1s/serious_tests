8052c69920bac6454bcd42447a0b8e2a2dbc90e4
Na1s <imna1se@gmail.com>
2022-02-16 17:16:22 +0500

    init

---
+++ core/Page_methods/operator_page.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.support.ui import Select
+
+from core.core_mt import BaseMt
+from core.links import LinksReqTds
+from core.locators import PartnerLocators, StreamLocators, TicketLocators
+from core.statistic.main_statistic_base import StatsMt
+
+class OperatorMT(BaseMt):
+    def
