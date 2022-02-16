9c670eefeb81ddbaf6cca4afb1cd7630e22cc832
Na1s <imna1se@gmail.com>
2022-02-16 18:38:05 +0500

    create class, and add almost ready method create operator

---
+++ core/Page_methods/operator_ready_mt.py
@@ -0,0 +1,17 @@
+import time
+
+from core.Page_methods.operator_page import OperatorMT
+
+
+class OperatorReadyMt:
+    def opeartor_create(self, link):
+        Page = OperatorMT(self, link)
+        Page.operator_page_open()
+        Page.operator_create_button()
+        Page.operator_give_name()
+        Page.operator_subnet()
+        Page.operator_denied_devices()
+        Page.operator_country()
+        Page.operator_status()
+        Page.operator_web()
+        Page.operator_create_end_button()
