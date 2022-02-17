76901f534dabc21e9cfb9e5ef02968655a3fa713
Na1s <imna1se@gmail.com>
2022-02-17 15:23:26 +0500

    init, add methods for interact with country page

---
+++ core/page_methods/country_page.py
@@ -0,0 +1,25 @@
+
+from core.core_mt import BaseMt
+from core.locators import CountryLocators, AdmSideBarLocators
+
+
+class CountryMt(BaseMt):
+    def country_page_open(self):
+        assert self.is_element_present(*AdmSideBarLocators.landing_menu), 'can\'t find landing'
+        self.find_el_click(*AdmSideBarLocators.landing_menu)
+        assert self.is_element_present(*AdmSideBarLocators.country_menu), 'can\'t find button country'
+        self.find_el_click(*AdmSideBarLocators.country_menu)
+
+    def country_create_button(self):
+        assert self.is_element_present(*CountryLocators.country_add), 'can\'t find button create country'
+        self.find_el_click(CountryLocators.country_add)
+
+    def country_name(self):
+        assert self.is_element_present(*CountryLocators.country_name), 'can\'t find field name'
+        self.find_el_write(*CountryLocators.country_name, 'autotest')
+
+    def country_iso(self):
+        assert self.is_element_present(*CountryLocators.country_iso), 'can\'t find field ISO'
+        self.find_el_write(*CountryLocators.country_iso, 'AUTO')
+
+    
