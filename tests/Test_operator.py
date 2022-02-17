3e102bffd22163a80367856533a766767e46d132
Na1s <imna1se@gmail.com>
2022-02-17 13:05:52 +0500

    add marks

--- tests/Test_operator.py
+++ tests/Test_operator.py
@@ -1,9 +1,13 @@
+import pytest
+
 from core.links import SiteLinks
 from core.page_methods.operator_ready_mt import OperatorReadyMt
 from core.page_methods.ready_pages_mt import BrowserMt
 
 
-def test_js(browser):
+@pytest.mark.operator
+@pytest.mark.create
+def test_operator(browser):
     link = SiteLinks.login_link_test1
     BrowserMt.login_pass(browser, link)
     OperatorReadyMt.operator_create(browser, link)
