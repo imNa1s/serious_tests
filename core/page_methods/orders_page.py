ae7f1c3cb9cca13595b95a9c59925f9ce73b30e3
Na1s <imna1se@gmail.com>
2022-02-18 14:55:59 +0500

    add method to open orders page

---
+++ core/page_methods/orders_page.py
@@ -0,0 +1,10 @@
+from core.core_mt import BaseMt
+from core.locators import AdmSideBarLocators
+
+
+class AdmLandingPage(BaseMt):
+    def lp_page_open(self):
+        assert self.is_element_present(*AdmSideBarLocators.payments_menu), 'can\'t find button "payments"'
+        self.find_el_click(*AdmSideBarLocators.payments_menu)
+        assert self.is_element_present(*AdmSideBarLocators.orders_menu), 'can\'t find button "orders"'
+        self.find_el_click(*AdmSideBarLocators.orders_menu)
