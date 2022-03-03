428436614532f7a1293bfb5c15bffc1ca6471297
Na1s <imna1se@gmail.com>
2022-03-03 18:06:05 +0500

    add variable conftest

---
+++ conftest_headless.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    chromeOptions = Options()
+    chromeOptions.add_argument("--headless")
+    browser = webdriver.Chrome(options=chromeOptions)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
