13dfb1ab2f629024f9dce416e86bc3171290cde2
Na1s <imna1se@gmail.com>
2022-03-14 19:53:12 +0500

    refactoring chrome options

--- conftest_selenoid.py
+++ conftest_selenoid.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope="function")
 def browser():
     chromeOptions = Options()
+    chromeOptions.add_argument()
     browser = webdriver.Remote(
         command_executor='http://localhost:4444/wd/hub',
         desired_capabilities={'browserName': 'chrome',
