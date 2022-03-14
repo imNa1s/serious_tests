3e3806b1f4f14429eadc5502014fdae153ff90a1
Na1s <imna1se@gmail.com>
2022-03-14 19:54:33 +0500

    refactoring chrome options

--- conftest_selenoid.py
+++ conftest_selenoid.py
@@ -1,17 +1,15 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function")
 def browser():
-    chromeOptions = Options()
-    chromeOptions.add_argument()
+    chrome_options = webdriver.ChromeOptions()
     browser = webdriver.Remote(
         command_executor='http://localhost:4444/wd/hub',
         desired_capabilities={'browserName': 'chrome',
                               'version': '99.0'},
-        options=chromeOptions)
+        options=chrome_options)
 
     browser.maximize_window()
 
